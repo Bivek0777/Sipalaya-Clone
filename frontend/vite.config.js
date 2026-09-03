385a4350d3b2743d33d7a29312b646a65c89f26e
bibekchaudhary-source <chaudharybivek75@gmail.com>
2026-09-03 17:52:58 +0545

    Optimize image handling

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 import tailwindcss from '@tailwindcss/vite'
+import imagemin from 'vite-plugin-imagemin'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), imagemin({
+    include: ['**/*.{png,jpg,jpeg,svg,webp}'],
+    limit: 10240, // only optimize images > 10KB
+    gzipSize: true,
+    brotliSize: true,
+  })],
   define: {
     global: 'window',
   },
